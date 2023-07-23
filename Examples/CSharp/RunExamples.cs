41dd4631599dd6d74f7f7099cfac91e84de36cf3
alexander.feigin@aspose.com <alexander.feigin@aspose.com>
2023-07-23 19:53:53 +0300

    Fix formatting

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -2,89 +2,89 @@
 
 namespace Aspose.Drawing.Examples.CSharp
 {
-    class RunExamples
-    {
-        static void Main(string[] args)
-        {
-            //ConvertImage.Run();
+	class RunExamples
+	{
+		static void Main(string[] args)
+		{
+			//ConvertImage.Run();
 
-            // Use a path to license file as a parameter if you have Aspose.Drawing license:
-            if(args.Length>0 && File.Exists(args[0]))
-            {
+			// Use a path to license file as a parameter if you have Aspose.Drawing license:
+			if (args.Length > 0 && File.Exists(args[0]))
+			{
 				LoadLicenseFromFile(args[0]);
-			}            
+			}
 
-            UseCases.TextOnImage.Run();
+			UseCases.TextOnImage.Run();
 			UseCases.PhotoFrame.Run();
 			UseCases.MakeCallOut.Run();
 
-            LinesCurvesShapes.DrawLines.Run();
-            LinesCurvesShapes.DrawRectangle.Run();
-            LinesCurvesShapes.DrawEllipse.Run();
-            LinesCurvesShapes.DrawArc.Run();
-            LinesCurvesShapes.DrawPolygon.Run();
-            LinesCurvesShapes.DrawCardinalSpline.Run();
-            LinesCurvesShapes.DrawBezierSpline.Run();
-            LinesCurvesShapes.DrawClosedCurve.Run();
-            LinesCurvesShapes.DrawPath.Run();
-            LinesCurvesShapes.FillRegion.Run();
+			LinesCurvesShapes.DrawLines.Run();
+			LinesCurvesShapes.DrawRectangle.Run();
+			LinesCurvesShapes.DrawEllipse.Run();
+			LinesCurvesShapes.DrawArc.Run();
+			LinesCurvesShapes.DrawPolygon.Run();
+			LinesCurvesShapes.DrawCardinalSpline.Run();
+			LinesCurvesShapes.DrawBezierSpline.Run();
+			LinesCurvesShapes.DrawClosedCurve.Run();
+			LinesCurvesShapes.DrawPath.Run();
+			LinesCurvesShapes.FillRegion.Run();
 
-            Pens.Colors.Run();
-            Pens.Width.Run();
-            Pens.Join.Run();
+			Pens.Colors.Run();
+			Pens.Width.Run();
+			Pens.Join.Run();
 
-            Brushes.Solid.Run();
+			Brushes.Solid.Run();
 
-            TextFonts.InstalledFonts.Run();
-            TextFonts.DrawText.Run();
-            TextFonts.FormatText.Run();
-            TextFonts.Hinting.Run();
+			TextFonts.InstalledFonts.Run();
+			TextFonts.DrawText.Run();
+			TextFonts.FormatText.Run();
+			TextFonts.Hinting.Run();
 
-            Rendering.Antialiasing.Run();
-            Rendering.Clipping.Run();
-            Rendering.AlphaBlending.Run();
+			Rendering.Antialiasing.Run();
+			Rendering.Clipping.Run();
+			Rendering.AlphaBlending.Run();
 
-            CoordinateSystemsTransformations.WorldTransformation.Run();
-            CoordinateSystemsTransformations.PageTransformation.Run();
-            CoordinateSystemsTransformations.UnitsOfMeasure.Run();
-            CoordinateSystemsTransformations.MatrixTransformations.Run();
-            CoordinateSystemsTransformations.GlobalTransformation.Run();
-            CoordinateSystemsTransformations.LocalTransformation.Run();
+			CoordinateSystemsTransformations.WorldTransformation.Run();
+			CoordinateSystemsTransformations.PageTransformation.Run();
+			CoordinateSystemsTransformations.UnitsOfMeasure.Run();
+			CoordinateSystemsTransformations.MatrixTransformations.Run();
+			CoordinateSystemsTransformations.GlobalTransformation.Run();
+			CoordinateSystemsTransformations.LocalTransformation.Run();
 
-            Images.LoadSave.Run();
-            Images.Display.Run();
-            Images.Scale.Run();
-            Images.Cropping.Run();
-            Images.DirectDataAccess.Run();
+			Images.LoadSave.Run();
+			Images.Display.Run();
+			Images.Scale.Run();
+			Images.Cropping.Run();
+			Images.DirectDataAccess.Run();
 
-            GraphicsFileFormats.LoadSave.Run();
-        }
+			GraphicsFileFormats.LoadSave.Run();
+		}
 
-        public static string GetDataDir()
-        {
-            var parent = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
-            string startDirectory = null;
-            if (parent != null)
-            {
-                var directoryInfo = parent.Parent;
-                if (directoryInfo != null)
-                {
-                    startDirectory = directoryInfo.FullName;
-                }
-            }
-            else
-            {
-                startDirectory = parent.FullName;
-            }
-            return Path.Combine(startDirectory, "Data\\");
-        }
+		public static string GetDataDir()
+		{
+			var parent = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+			string startDirectory = null;
+			if (parent != null)
+			{
+				var directoryInfo = parent.Parent;
+				if (directoryInfo != null)
+				{
+					startDirectory = directoryInfo.FullName;
+				}
+			}
+			else
+			{
+				startDirectory = parent.FullName;
+			}
+			return Path.Combine(startDirectory, "Data\\");
+		}
 
-        private static void LoadLicenseFromFile(string licensePath)
-        {
+		private static void LoadLicenseFromFile(string licensePath)
+		{
 			// Initialize license object
 			System.Drawing.AsposeDrawing.License license = new System.Drawing.AsposeDrawing.License();
-            // Set license
-            license.SetLicense(licensePath);
-        }
-    }
+			// Set license
+			license.SetLicense(licensePath);
+		}
+	}
 }
