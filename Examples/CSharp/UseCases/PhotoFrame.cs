0b7130ce9f24da2649e3b56a5041bd3a56f58a55
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-08-09 21:23:55 +0500

    Corrected UseCases output file names.

--- Examples/CSharp/UseCases/PhotoFrame.cs
+++ Examples/CSharp/UseCases/PhotoFrame.cs
@@ -9,27 +9,27 @@ using System.Threading.Tasks;
 
 namespace Aspose.Drawing.Examples.CSharp.UseCases
 {
-	internal class PhotoFrame
-	{
-		public static void Run()
-		{
-			// Create photo frame
-			using (var image = Image.FromFile(Path.Combine(RunExamples.GetDataDir(), "UseCases", "cat.jpg")))
-			{
-				var graphics = Graphics.FromImage(image);
+    internal class PhotoFrame
+    {
+        public static void Run()
+        {
+            // Create photo frame
+            using (var image = Image.FromFile(Path.Combine(RunExamples.GetDataDir(), "UseCases", "cat.jpg")))
+            {
+                var graphics = Graphics.FromImage(image);
 
-				graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-				graphics.PageUnit = GraphicsUnit.Pixel;
+                graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                graphics.PageUnit = GraphicsUnit.Pixel;
 
-				var pen = new Pen(Color.Magenta, 1);
+                var pen = new Pen(Color.Magenta, 1);
 
-				int gap = 2;
+                int gap = 2;
 
-				graphics.DrawRectangle(pen, 0, 0, image.Width - 1, image.Height - 1);
-				graphics.DrawRectangle(pen, gap, gap, image.Width - gap - 1, image.Height - gap - 1);
+                graphics.DrawRectangle(pen, 0, 0, image.Width - 1, image.Height - 1);
+                graphics.DrawRectangle(pen, gap, gap, image.Width - gap - 1, image.Height - gap - 1);
 
-				image.Save(Path.Combine(RunExamples.GetDataDir(), "UseCases", "cat_with_honor.jpg"));
-			}
-		}
-	}
+                image.Save(Path.Combine(RunExamples.GetDataDir(), "UseCases", "cat_with_honor_out.jpg"));
+            }
+        }
+    }
 }
--- Examples/CSharp/UseCases/TextOnImage.cs
+++ Examples/CSharp/UseCases/TextOnImage.cs
@@ -6,35 +6,35 @@ using System.Linq;
 
 namespace Aspose.Drawing.Examples.CSharp.UseCases
 {
-	internal class TextOnImage
-	{
-		public static void Run()
-		{
-			using (var image = Image.FromFile(Path.Combine(RunExamples.GetDataDir(), "UseCases", "girl.jpg")))
-			{
-				var graphics = Graphics.FromImage(image);
+    internal class TextOnImage
+    {
+        public static void Run()
+        {
+            using (var image = Image.FromFile(Path.Combine(RunExamples.GetDataDir(), "UseCases", "girl.jpg")))
+            {
+                var graphics = Graphics.FromImage(image);
 
-				graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-				graphics.PageUnit = GraphicsUnit.Pixel;
+                graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                graphics.PageUnit = GraphicsUnit.Pixel;
 
-				SolidBrush brush = new SolidBrush(Color.Navy);
-				Font font = new Font("Calibri", 20, FontStyle.Italic);
+                SolidBrush brush = new SolidBrush(Color.Navy);
+                Font font = new Font("Calibri", 20, FontStyle.Italic);
 
-				int padding = 5;
+                int padding = 5;
 
-				string text = "Happy Birthday!";
-				var words = text.Split(' ');
+                string text = "Happy Birthday!";
+                var words = text.Split(' ');
 
-				int extentWidth = 0;
-				int extentHeight = 0;
+                int extentWidth = 0;
+                int extentHeight = 0;
 
-				words.ToList().ForEach(word => { var stringSize = graphics.MeasureString(word, font); extentWidth = Math.Max(extentWidth, (int)stringSize.Width + padding); extentHeight += (int)stringSize.Height; });
+                words.ToList().ForEach(word => { var stringSize = graphics.MeasureString(word, font); extentWidth = Math.Max(extentWidth, (int)stringSize.Width + padding); extentHeight += (int)stringSize.Height; });
 
-				Rectangle rectangle = new Rectangle(image.Width - padding - extentWidth, image.Height - padding - extentHeight, extentWidth, extentHeight);
-				graphics.DrawString(text, font, brush, rectangle);
+                Rectangle rectangle = new Rectangle(image.Width - padding - extentWidth, image.Height - padding - extentHeight, extentWidth, extentHeight);
+                graphics.DrawString(text, font, brush, rectangle);
 
-				image.Save(Path.Combine(RunExamples.GetDataDir(), "UseCases", "girl_card.jpg"));
-			}
-		}
-	}
+                image.Save(Path.Combine(RunExamples.GetDataDir(), "UseCases", "girl_card_out.jpg"));
+            }
+        }
+    }
 }
