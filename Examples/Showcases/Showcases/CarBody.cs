01919faaf737f61069ce331b4d9b34d7602a7d2d
Dmitry Tolmachyov <dmitry.tolmachyov@gmail.com>
2023-08-27 22:20:56 +0500

    Remake CarBody for HD 1080

--- Examples/Showcases/Showcases/CarBody.cs
+++ Examples/Showcases/Showcases/CarBody.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Drawing.Text;
@@ -8,11 +7,13 @@ namespace Aspose.Drawing.Showcases
 {
     internal class CarBody
     {
-        private static Brush edgeBrush = Brushes.Magenta;
+        private static readonly bool makeVideo = false;
+        private static readonly Brush edgeBrush = Brushes.Magenta;
         private static readonly string rootDirectory = Path.Combine(RunShowcases.GetDataDir(), "CarBody");
         private static readonly string outputDirectory = Path.Combine(rootDirectory, "out");
         private static readonly string inputDirectory = rootDirectory;
-        private static List<Bitmap> frames = new List<Bitmap>();
+        private static readonly List<Bitmap> frames = new();
+        private static readonly List<Skin> skins = new();
 
         public static void Run()
         {
@@ -21,42 +22,44 @@ namespace Aspose.Drawing.Showcases
 
             GraphicsPath carBody = MakeCarBody(h);
 
-            frames.Add(
-                DrawFrame(
-                    w, h, carBody,
-                    Color.FromArgb(120, 0, 80, 80),
-                    Color.White, "", 0, 0, null, null, 0, 0));
+            Skin skin1 = new() { Background = Color.FromArgb(120, 0, 80, 80) };
+            skins.Add(skin1);
 
-            frames.Add(
-                DrawFrame(
-                    w, h, carBody,
-                    Color.FromArgb(80, 180, 80, 0),
-                    Color.White, "", 0, 0, null, null, 0, 0));
+            Skin skin2 = new() { Background = Color.FromArgb(80, 180, 80, 0) };
+            skins.Add(skin2);
 
-            frames.Add(
-                DrawFrame(
-                    w, h, carBody,
-                    Color.FromArgb(80, 80, 80, 180),
-                    Color.FromArgb(120, 0, 255, 255), "KEWIN", 350, h - 277,
-                    new Font("Arial", 140, FontStyle.Bold, GraphicsUnit.Pixel),
-                    Path.Combine(inputDirectory, "brit-01.png"), 333, h - 253));
+            Skin skin3 = new() { Background = Color.FromArgb(80, 80, 80, 180) };
+            skin3.Design.Add(new DesignElement(
+                new Logo(Path.Combine(inputDirectory, "brit-01.png"), new PointF(333, h - 253))));
+            skin3.Design.Add(new DesignElement(
+                new Banner("KEWIN", new Font("Arial", 140, FontStyle.Bold, GraphicsUnit.Pixel),
+                Color.FromArgb(120, 0, 255, 255), new PointF(350, h - 277))));
+            skins.Add(skin3);
 
-            frames.Add(
-                DrawFrame(
-                    w, h, carBody,
-                    Color.FromArgb(120, 255, 255, 0),
-                    Color.FromArgb(120, 255, 0, 0), "ANCHOR", 250, h - 285,
-                    new Font("Verdana", 120, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Pixel),
-                    Path.Combine(inputDirectory, "anchor-01.png"), 275, h - 290));
+            Skin skin4 = new() { Background = Color.FromArgb(120, 255, 255, 0) };
+            skin4.Design.Add(new DesignElement(
+                new Logo(Path.Combine(inputDirectory, "anchor-01.png"), new PointF(275, h - 290))));
+            skin4.Design.Add(new DesignElement(
+                new Banner("ANCHOR", new Font("Verdana", 120, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Pixel),
+                Color.FromArgb(120, 255, 0, 0), new PointF(250, h - 285))));
+            skins.Add(skin4);
+
+            foreach (Skin skin in skins)
+            {
+                frames.Add(DrawFrame(w, h, carBody, skin));
+            }
 
             float step = 0.5f;
-            float percent = 0;
+            float percent;
             int frameNumber = 0;
 
-            // tmp
-            Bitmap frame1 = frames[0];
-            Bitmap frame2 = frames[1];
-            Bitmap mixedFrame = Mix(w, h, frame1, frame2, percent);
+            Bitmap frame1;
+            Bitmap frame2;
+            Bitmap mixedFrame;
+
+            Bitmap frame = new(1920, 1080, PixelFormat.Format32bppPArgb);
+            Graphics g = Graphics.FromImage(frame);
+            g.SmoothingMode = SmoothingMode.HighQuality;
 
             for (int f1 = 0; f1 < frames.Count; f1++)
             {
@@ -64,34 +67,40 @@ namespace Aspose.Drawing.Showcases
                 frame1 = frames[f1];
                 frame2 = frames[f2];
 
-                for (int i = 0; i < 200; i++)
+                for (int i = 0; i < (makeVideo ? 200 : 1); i++)
                 {
                     ++frameNumber;
-                    percent = i * step % 100;
+                    percent = makeVideo ? i * step % 100 : 25;
                     mixedFrame = Mix(w, h, frame1, frame2, percent);
                     DrawEdge(w, h, carBody, mixedFrame, percent);
 
+                    for (int n = 0; n < 2; n++)
+                    {
+                        for (int m = 0; m < 3; m++)
+                        {
+                            g.DrawImage(mixedFrame, (1920 - 50) / 2 * n, (1080 - 50) / 3 * m - 40);
+                        }
+                    }
+
                     string fileName = string.Format("{0}/{1:d5}.png", outputDirectory, frameNumber);
-                    mixedFrame.Save(fileName);
+                    frame.Save(fileName);
                     Console.WriteLine(fileName);
                 }
             }
-
-            mixedFrame.Save(Path.Combine(inputDirectory, "carbody.png"));
         }
 
         private static Bitmap Mix(int w, int h, Bitmap frame1, Bitmap frame2, float percent)
         {
-            Bitmap bitmap = new Bitmap(w, h, PixelFormat.Format32bppPArgb);
+            Bitmap bitmap = new(w, h, PixelFormat.Format32bppPArgb);
             Graphics g = Graphics.FromImage(bitmap);
             g.SmoothingMode = SmoothingMode.HighQuality;
 
             float edgePosition = w * percent / 100;
 
-            RectangleF rect1 = new RectangleF(edgePosition, 0, w - edgePosition, h);
+            RectangleF rect1 = new(edgePosition, 0, w - edgePosition, h);
             g.DrawImage(frame1, rect1, rect1, GraphicsUnit.Pixel);
 
-            RectangleF rect2 = new RectangleF(0, 0, edgePosition, h);
+            RectangleF rect2 = new(0, 0, edgePosition, h);
             g.DrawImage(frame2, rect2, rect2, GraphicsUnit.Pixel);
 
             return bitmap;
@@ -102,71 +111,59 @@ namespace Aspose.Drawing.Showcases
             float edgeSize = 5.0f;
             float edgePosition = w * percent / 100;
 
-            Rectangle rect = new Rectangle(
+            Rectangle rect = new(
                 (int)(edgePosition - edgeSize / 2), 0,
                 (int)(edgeSize), h);
 
-            Region edge = new Region(carBody);
+            Region edge = new(carBody);
             edge.Intersect(rect);
 
             Graphics g = Graphics.FromImage(frame);        
             g.FillRegion(edgeBrush, edge);
         }
 
-        private static Bitmap DrawFrame(
-            int w, int h, GraphicsPath carBody,
-            Color bgColor,
-            Color textColor, string text, int textX, int textY, Font font,
-            string logoFile, int logoX, int logoY)
+        private static Bitmap DrawFrame(int w, int h, GraphicsPath carBody, Skin skin)
         {
-            Bitmap bitmap = new Bitmap(w, h, PixelFormat.Format32bppPArgb);
+            Bitmap bitmap = new(w, h, PixelFormat.Format32bppPArgb);
             Graphics g = Graphics.FromImage(bitmap);
             g.Clear(Color.White);
             g.SmoothingMode = SmoothingMode.HighQuality;
             g.TextRenderingHint = TextRenderingHint.AntiAlias;
 
-            Bitmap rasterCar = new Bitmap(Path.Combine(inputDirectory, "carbody-sedan-04.png"));
+            Bitmap rasterCar = new(Path.Combine(inputDirectory, "carbody-sedan-04.png"));
             g.DrawImage(rasterCar, new RectangleF(0, 0, w, h));
 
-
-
-            // draw car body
-            Brush brush1 = new SolidBrush(bgColor);
+            Brush brush1 = new SolidBrush(skin.Background);
             g.FillPath(brush1, carBody);
 
-
-
-            // make the pattern of painting for car body: logo + text (both optional)
-            if (!string.IsNullOrEmpty(logoFile) || !string.IsNullOrWhiteSpace(text))
+            if (skin.Design.Count > 0)
             {
-                Bitmap bitmap2 = new Bitmap(w, h, PixelFormat.Format32bppPArgb);
+                Bitmap bitmap2 = new(w, h, PixelFormat.Format32bppPArgb);
                 Graphics g2 = Graphics.FromImage(bitmap2);
                 g2.SmoothingMode = SmoothingMode.HighQuality;
                 g2.TextRenderingHint = TextRenderingHint.AntiAlias;
 
                 g2.Clip = new Region(carBody);
 
-                // logo
-                if (!string.IsNullOrEmpty(logoFile))
+                foreach (DesignElement de in skin.Design)
                 {
-                    Bitmap logo = new Bitmap(logoFile);
-                    g2.DrawImage(logo, new RectangleF(logoX, logoY, logo.Width, logo.Height));
+                    if (de.Banner is not null)
+                    {
+                        Brush brush2 = new SolidBrush(de.Banner.Color);
+                        g2.DrawString(de.Banner.Text, de.Banner.Font, brush2, de.Banner.Place.X, de.Banner.Place.Y);
+                    }
+
+                    if (de.Logo is not null)
+                    {
+                        Bitmap logo = new(de.Logo.ImageFile);
+                        g2.DrawImage(logo, new RectangleF(de.Logo.Place.X, de.Logo.Place.Y, logo.Width, logo.Height));
+                    }
                 }
 
-                // banner text
-                if (!string.IsNullOrWhiteSpace(text))
-                {
-                    Brush brush2 = new SolidBrush(textColor);
-                    g2.DrawString(text, font, brush2, textX, textY);
-                }
-
-
-
-                // painting of car body by pattern
                 g.Clip = new Region(carBody);
 
-                ImageAttributes imageAttributes = new ImageAttributes();
-                ColorMatrix colorMatrix = new ColorMatrix(new float[][]
+                ImageAttributes imageAttributes = new();
+                ColorMatrix colorMatrix = new(new float[][]
                 {
                     new float[] { 1, 0, 0, 0, 0 },
                     new float[] { 0, 1, 0, 0, 0 },
@@ -193,7 +190,7 @@ namespace Aspose.Drawing.Showcases
                     new RectangleF(0, 0, w, h),
                     GraphicsUnit.Pixel,
                     imageAttributes);
-            }
+            }         
 
             return bitmap;
         }
@@ -201,7 +198,7 @@ namespace Aspose.Drawing.Showcases
         private static GraphicsPath MakeCarBody(int h)
         {
             // external contour
-            GraphicsPath path1 = new GraphicsPath();
+            GraphicsPath path1 = new();
 
             // hood
             path1.AddBezier(
@@ -330,7 +327,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // headlight
-            GraphicsPath path2 = new GraphicsPath();
+            GraphicsPath path2 = new();
 
             // front line
             path2.AddBezier(
@@ -355,7 +352,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // rear lights
-            GraphicsPath path3 = new GraphicsPath();
+            GraphicsPath path3 = new();
 
             // front line
             path3.AddBezier(
@@ -387,7 +384,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // side glasses
-            GraphicsPath path4 = new GraphicsPath();
+            GraphicsPath path4 = new();
 
             // front line
             path4.AddBezier(
@@ -426,7 +423,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // windshield
-            GraphicsPath path5 = new GraphicsPath();
+            GraphicsPath path5 = new();
 
             // bottom line
             path5.AddBezier(
@@ -458,7 +455,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // rear glass
-            GraphicsPath path6 = new GraphicsPath();
+            GraphicsPath path6 = new();
 
             // top/rear line
             path6.AddBezier(
@@ -490,7 +487,7 @@ namespace Aspose.Drawing.Showcases
 
 
             // turn signal
-            GraphicsPath path7 = new GraphicsPath();
+            GraphicsPath path7 = new();
 
             path7.AddRectangle(new Rectangle(305, h - 227, 10, 5));
 
@@ -498,6 +495,56 @@ namespace Aspose.Drawing.Showcases
             path1.AddPath(path7, false);
 
             return path1;
+        }
+
+        internal class Banner
+        {
+            public string Text;
+            public Font Font;
+            public Color Color;
+            public PointF Place;
+
+            public Banner(string text, Font font, Color color, PointF place)
+            {
+                this.Text = text;
+                this.Font = font;
+                this.Color = color;
+                this.Place = place;
+            }
+        }
+
+        internal class Logo
+        {
+            public string ImageFile;
+            public PointF Place;
+
+            public Logo(string imageFile, PointF place)
+            {
+                this.ImageFile = imageFile;
+                this.Place = place;
+            }
+        }
+
+        internal class DesignElement
+        {
+            public Banner? Banner;
+            public Logo? Logo;
+
+            public DesignElement(Banner banner)
+            {
+                this.Banner = banner;
+            }
+
+            public DesignElement(Logo logo)
+            {
+                this.Logo = logo;
+            }
+        }
+
+        internal class Skin
+        {
+            public Color Background = new();
+            public List<DesignElement> Design = new();
         }
     }
 }
